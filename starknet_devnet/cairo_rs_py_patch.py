ead7f801d09a9e9cf0037c02bead408306f6e371
Federica <fedemoletta@hotmail.com>
2023-03-27 14:59:07 -0300

    Add execute override

--- starknet_devnet/cairo_rs_py_patch.py
+++ starknet_devnet/cairo_rs_py_patch.py
@@ -23,7 +23,7 @@ from starkware.cairo.lang.compiler.program import Program
 from starkware.cairo.lang.compiler.scoped_name import ScopedName
 from starkware.cairo.lang.vm.memory_segments import MemorySegmentManager
 from starkware.cairo.lang.vm.relocatable import MaybeRelocatable
-from starkware.cairo.lang.vm.utils import ResourcesError
+from starkware.cairo.lang.vm.utils import ResourcesError, RunResources
 from starkware.cairo.lang.vm.vm_exceptions import (
     SecurityError,
     VmException,
@@ -31,7 +31,7 @@ from starkware.cairo.lang.vm.vm_exceptions import (
 )
 from starkware.python.utils import safe_zip
 from starkware.starknet.business_logic.execution.execute_entry_point import (
-    FAULTY_CLASS_HASH,
+    EntryPointArgs,
     ExecuteEntryPoint,
 )
 from starkware.starknet.business_logic.execution.objects import (
@@ -58,6 +58,113 @@ from starkware.starkware_utils.error_handling import (
 )
 
 logger = logging.getLogger(__name__)
+
+
+def cairo_rs_py_execute(
+    self,
+    state: SyncState,
+    compiled_class: CompiledClass,
+    class_hash: int,
+    resources_manager: ExecutionResourcesManager,
+    general_config: StarknetGeneralConfig,
+    tx_execution_context: TransactionExecutionContext,
+    support_reverted: bool,
+) -> CallInfo:
+    # Fix the current resources usage, in order to calculate the usage of this run at the end.
+    previous_cairo_usage = resources_manager.cairo_usage
+
+    # Create a dummy layout.
+    layout = dataclasses.replace(
+        STARKNET_LAYOUT_INSTANCE,
+        builtins={**STARKNET_LAYOUT_INSTANCE.builtins, "segment_arena": {}},
+    )
+
+    # Prepare runner.
+    entry_point = self._get_selected_entry_point(
+        compiled_class=compiled_class, class_hash=class_hash
+    )
+    program = compiled_class.get_runnable_program(
+        entrypoint_builtins=as_non_optional(entry_point.builtins)
+    )
+    with wrap_with_stark_exception(code=StarknetErrorCode.SECURITY_ERROR):
+        runner = cairo_rs_py.CairoRunner(  # pylint: disable=no-member
+            program=program.dumps()
+        )
+    runner.initialize_function_runner(add_segment_arena_builtin=True)
+
+    # Prepare implicit arguments.
+    implicit_args = os_utils.prepare_os_implicit_args(
+        runner=runner, gas=self.initial_gas
+    )
+
+    # Prepare syscall handler.
+    initial_syscall_ptr = cast(RelocatableValue, implicit_args[-1])
+    syscall_handler = BusinessLogicSyscallHandler(
+        state=state,
+        resources_manager=resources_manager,
+        segments=runner.segments,
+        tx_execution_context=tx_execution_context,
+        initial_syscall_ptr=initial_syscall_ptr,
+        general_config=general_config,
+        entry_point=self,
+        support_reverted=support_reverted,
+    )
+
+    # Load the builtin costs; Cairo 1.0 programs are expected to end with a `ret` opcode
+    # followed by a pointer to the builtin costs.
+    core_program_end_ptr = runner.program_base + len(program.data)
+    builtin_costs = [0, 0, 0, 0, 0]
+    # Use allocate_segment to mark it as read-only.
+    builtin_cost_ptr = syscall_handler.allocate_segment(data=builtin_costs)
+    program_extra_data: List[MaybeRelocatable] = [0x208B7FFF7FFF7FFE, builtin_cost_ptr]
+    runner.load_data(ptr=core_program_end_ptr, data=program_extra_data)
+
+    # Arrange all arguments.
+
+    # Allocate and mark the segment as read-only (to mark every input array as read-only).
+    calldata_start = syscall_handler.allocate_segment(data=self.calldata)
+    calldata_end = calldata_start + len(self.calldata)
+    entry_point_args: EntryPointArgs = [
+        # Note that unlike old classes, implicit arguments appear flat in the stack.
+        *implicit_args,
+        calldata_start,
+        calldata_end,
+    ]
+
+    # Run.
+    self._run(
+        runner=runner,
+        entry_point_offset=entry_point.offset,
+        entry_point_args=entry_point_args,
+        hint_locals={"syscall_handler": syscall_handler},
+        run_resources=tx_execution_context.run_resources,
+        program_segment_size=len(runner.program.data) + len(program_extra_data),
+        allow_tmp_segments=True,
+    )
+
+    # We should not count (possibly) unsued code as holes.
+    runner.mark_as_accessed(address=core_program_end_ptr, size=len(program_extra_data))
+
+    # Complete validations.
+    os_utils.validate_and_process_os_implicit_args(
+        runner=runner,
+        syscall_handler=syscall_handler,
+        initial_implicit_args=implicit_args,
+    )
+
+    # Update resources usage (for the bouncer and fee calculation).
+    resources_manager.cairo_usage += runner.get_execution_resources()
+
+    # Build and return the call info.
+    return self._build_call_info(
+        class_hash=class_hash,
+        execution_resources=resources_manager.cairo_usage - previous_cairo_usage,
+        storage=syscall_handler.storage,
+        result=get_call_result(runner=runner, initial_gas=self.initial_gas),
+        events=syscall_handler.events,
+        l2_to_l1_messages=syscall_handler.l2_to_l1_messages,
+        internal_calls=syscall_handler.internal_calls,
+    )
 
 
 def cairo_rs_py_run(
@@ -339,6 +446,7 @@ def cairo_rs_py_validate_segment_pointers(
 
 
 def cairo_rs_py_monkeypatch():
+    setattr(ExecuteEntryPoint, "_execute", cairo_rs_py_execute)
     setattr(ExecuteEntryPoint, "_run", cairo_rs_py_run)
     setattr(
         sys.modules["starkware.starknet.core.os.class_hash"],
